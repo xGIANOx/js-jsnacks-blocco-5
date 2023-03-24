c124f1d8b630605e9e0b69565fd853d144f35bcf
xGIANOx <studioenricogian@gmail.com>
2023-03-24 12:36:16 +0100

    initial commit

---
+++ JSnack5/main.js
@@ -0,0 +1,5 @@
+/* 
+Scrivi una funzione che accetti tre argomenti:
+un array e due numeri ("a" più piccolo di "b" e "b" grande al massimo quanto il numero di elementi dell'array).
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra "a" e "b"
+*/
